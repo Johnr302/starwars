8fbde5b19ea1414e332420b0c171468debb62904
Johnr302 <johnathon.regalado@gmail.com>
2020-09-18 20:07:42 -0700

    changing url to https from http

--- src/shared/service.js
+++ src/shared/service.js
@@ -2,6 +2,8 @@
 // that then runs a custom function on the data that gets returned
 
 const fetchRequest = (url, customThenFunction) => {
+  url.replace(/https|http/gi, "https");
+  console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then(customThenFunction);
